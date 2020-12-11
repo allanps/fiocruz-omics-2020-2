d57a2600e74c16b20375acfc5809e7eba98dd57e
Allan <allanpaulo2@hotmail.com>
2020-12-11 10:27:53 -0300

    Entities created

---
+++ entities/Dataset.py
@@ -0,0 +1,7 @@
+class Dataset():
+    def __init__(self,userID,techniqueID,title,description,url):
+        self.userID = userID
+        self.techniqueID = techniqueID
+        self.title = title
+        self.description = description
+        self.url = url
---
+++ entities/Model.py
@@ -0,0 +1,6 @@
+class Model():
+    def __init__(self,userID,title,description,url):
+        self.userID = userID
+        self.title = title
+        self.description = description
+        self.url = url
